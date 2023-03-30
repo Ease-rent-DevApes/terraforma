04b06cb2f49fc9dd728c08bfec64af009e267d96
zarigata <vallestrauch@yahoo.com>
2023-03-30 15:46:20 -0300

    mod.sh added

--- run.sh
+++ run.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
 # Set the directory containing the Bash files
-directory="scripts"
+directory="./scripts"
 
 # Get the list of Bash files in the directory
 files=$(find "$directory" -name "*.sh" -type f)
 
 # Print the list of files and ask the user which ones to run
-echo -e "\e[44mSelect the scripts to run:\e[0m"
-echo -e "\e[44ma) All scripts\e[0m"
-i=1
+echo "Select the scripts to run:"
+echo "a) All scripts"
 for file in $files; do
-  echo -e "\e[44m$i) $(basename "$file")\e[0m"
-  ((i++))
+  echo "$(basename "$file")"
 done
-read -rp $'\e[44mEnter the script numbers (separated by spaces): \e[0m' choices
+read -rp "Enter the script numbers (separated by spaces): " choices
 
 # Execute the selected scripts
 for choice in $choices; do
   if [[ "$choice" == "a" ]]; then
     for file in $files; do
-      echo -e "\e[44mRunning $(basename "$file")...\e[0m"
       bash "$file"
     done
   else
     file="$directory/$(sed -n "${choice}p" <<< "$files")"
     if [[ -x "$file" ]]; then
-      echo -e "\e[44mRunning $(basename "$file")...\e[0m"
       bash "$file"
     else
-      echo -e "\e[41mError: $file is not executable.\e[0m"
+      echo "Error: $file is not executable."
     fi
   fi
 done
