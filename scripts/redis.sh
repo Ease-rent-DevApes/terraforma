5de5939bd13fc7b610adf919e0375c12d1b4a5b0
zarigata <vallestrauch@yahoo.com>
2023-03-31 00:38:52 -0300

    EM CASA

--- scripts/redis.sh
+++ scripts/redis.sh
@@ -43,3 +43,16 @@ echo "                                        :@+                               
 echo "                                         @.                                               "
 echo "                                         *                                                "
 echo " ========================   E A S E .  R E N T  DevOps Script    ========================="
+
+kubectl create ns redis-cc
+
+kubectl apply -n redis-cc -f deploy/bitnami/redis/cert.yaml
+
+kubectl get cert -n redis-cc
+
+export REDIS_PASSWORD=$((echo "@" && (openssl rand -base64 64 | tr -d '\n' | tr -d '==') && echo "@") | tr -d "[:space:]")
+
+helm install redis-cc --namespace redis-cc --create-namespace
+-f deploy/bitnami/redis/redis.yaml
+--set auth.password=$REDIS_PASSWORD
+bitnami/redis
