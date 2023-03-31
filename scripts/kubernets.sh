ff0d56646571d6b286c8c5ffff51cca2e10738df
zarigata <vallestrauch@yahoo.com>
2023-03-30 21:30:02 -0300

    LAST TROFEU?

--- scripts/kubernets.sh
+++ scripts/kubernets.sh
@@ -46,7 +46,22 @@ echo " ========================   E A S E .  R E N T  DevOps Script    =========
 
 
 
+kubeadm init --apiserver-advertise-address $(hostname -i)
 
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+mkdir -p $HOME/.kube
+cat /etc/kubernetes/admin.conf > $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+echo "Instalando Cloud Weave"
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+kubectl taint nodes $(hostname) node-role.kubernetes.io/master-
+kubectl taint nodes $(hostname) node-role.kubernetes.io/control-plane-
+
+systemctl restart kubelet
 
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
