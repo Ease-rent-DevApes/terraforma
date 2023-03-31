96e80d501e0a557fb16244ddbfcbf8cb6c76d323
zarigata <vallestrauch@yahoo.com>
2023-03-30 21:35:21 -0300

    kubernets

--- scripts/kubernets.sh
+++ scripts/kubernets.sh
@@ -44,7 +44,27 @@ echo "                                         @.                               
 echo "                                         *                                                "
 echo " ========================   E A S E .  R E N T  DevOps Script    ========================="
 
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
 
+echo "{\"exec-opts\": [\"native.cgroupdriver=systemd\"]}" > /etc/docker/daemon.json
+systemctl daemon-reload
+systemctl restart docker
+systemctl restart kubelet
+
+hostnamectl set-hostname
+hostname -i
+
+echo "Desativando swap"
+
+swapoff -a
+sed -e '/swapfile/ s/^#*/#/' -i /etc/fstab
+
+rm /etc/containerd/config.toml
+systemctl restart containerd
 
 kubeadm init --apiserver-advertise-address $(hostname -i)
 
